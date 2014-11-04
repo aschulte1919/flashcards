639bcda3001642e40811b1ac788029693a39408f
aschulte1919 <aschulte@nebrwesleyan.edu>
2014-11-04 10:47:11 -0500

    a lot of testing junk

--- index.js
+++ index.js
@@ -1,3 +1,50 @@
 "use strict"; // This line chooses a JavaScript dialect, one that helps both jsLint (used in OrionHub) and browsers catch errors.
 /*jslint browser: true*/ // This line tells jsLint that the code will run in a browser.
 
+var questions = [
+    {
+	question: 'WHat color is this dog?',
+	options: ['A', 'B', 'D'],
+	answer: ['D'],
+    },
+    {
+	question: 'What is the answer to this second question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this third question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this fourth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this fifth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this sixth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this seventh question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this eighth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+    {
+	question: 'What is the answer to this ninth question?',
+	options: ['This choice', 'This other choice', 'This yet another choice'],
+	answer: ['This choice'],
+    },
+];
