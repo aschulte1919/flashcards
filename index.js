f36a0d2f8d2acdc58059cd9c5bd63e8b2a2fecab
aschulte1919 <aschulte@nebrwesleyan.edu>
2014-11-11 10:40:06 -0500

    thomas

--- index.js
+++ index.js
@@ -17,17 +17,12 @@ var options =['Apple', 'Pizza', 'Carrot', 'Borscht', 'Ice Cream', 'Falafel', 'Fr
 
 var image_options =['http://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg', 'http://images6.alphacoders.com/412/412067.jpg', 'https://extension.umass.edu/vegetable/sites/vegetable/files/crops/carrot1.jpg', 'http://cdn.noshon.it/wp-content/uploads/2013-11-04-r-simple-roasted-beet-borscht1.jpg', 'http://continentalicecream.com/wp-content/uploads/2013/10/savoury-Ice-Cream-Cones.jpg', 'http://www.oasisde.com/restaurant/wp-content/uploads/2011/02/falafel-sandwich.jpg', 'http://www.chiantilvpa.com/wp-content/uploads/2014/09/French-fries-deliciouse.jpg', 'http://www.aperitif.no/var/aperitif/storage/images/oppskrifter2/sesongmat/julemenyen/alt-om-lutefisk/302612-23-nor-NO/Alt-om-lutefisk.jpg', 'http://i443.photobucket.com/albums/qq153/jlocklin_photos/Food%20Shots/Beef%20Stroganoff/IMG_4707.jpg', 'http://www.sarahwilson.com.au/wp-content/uploads/2012/03/cream_cheese_16x9.jpg', 'http://know.burrp.com/wp-content/uploads/2012/11/2-KUTCHI-GOSHT-BIRYANI.jpg']
 
-function Image_return() {
-	document.getElementById('A').img src = Random_image();
-	document.getElementById('B').img src = Random_image();
-	document.getElementById('C').img src = Random_image();
-}	
+	
 function Random_image() {
 	var random_photo;
 	random_photo = Math.random() * image_options.length; 
 	random_photo = Math.floor(random_photo);
 	random_photo = options[random_photo] ;
-	hold = random_photo
 	return random_photo;
 }
 
@@ -35,6 +30,7 @@ function generate_question() {
 	document.getElementById('A').innerHTML = Random_word();
 	document.getElementById('B').innerHTML = Random_word();
 	document.getElementById('C').innerHTML = Random_word();
+	document.getElementById('thomas').src= Random_image();
 }	
 function Random_word() {
 	var random_answer;
@@ -47,4 +43,4 @@ function Random_word() {
 
 generate_question();
 
-var hold 
+var hold;
