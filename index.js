551a830a9c312554f7d39a64808695afb0a9e527
aschulte1919 <aschulte@nebrwesleyan.edu>
2014-11-20 11:11:07 -0500

    letter grade

--- index.js
+++ index.js
@@ -16,16 +16,30 @@ function correct_answer() {
 }
 
 function incorrect_answer() {
+	var percent = Math.round(total_correct / total_count*100);
     document.getElementById('response').innerHTML = 'Incorrect'; 
     window.setTimeout(generate_question, 1000);
     total_count = total_count +1;
     total_correct = total_correct;
    document.getElementById('terry').innerHTML = total_correct;
    document.getElementById('lee').innerHTML = total_count;
-   document.getElementById('tod').innerHTML = Math.round(total_correct / total_count*100);  
-}
-   
-   
+   document.getElementById('tod').innerHTML = percent;  
+if (percent < 90 && percent > 80){
+  document.getElementById('matt').innerHTML = 'B'; 
+  }
+if (percent < 100 && percent > 90){
+  document.getElementById('matt').innerHTML = 'A'; 
+  }
+  if (percent < 80 && percent > 70){
+  document.getElementById('matt').innerHTML = 'C'; 
+  }
+    if (percent < 70 && percent > 60){
+  document.getElementById('matt').innerHTML = 'D'; 
+  }
+      if (percent < 60){
+  document.getElementById('matt').innerHTML = 'F'; 
+  }
+} 
 var options = ['Apple', 'Pizza', 'Carrot', 'Borscht', 'Ice Cream', 'Falafel', 'French Fries', 'Lutefisk', 'Beef Stroganoff', 'Cream Cheese', 'Biryani'];
 
 var image_options = ['http://upload.wikimedia.org/wikipedia/commons/1/15/Red_Apple.jpg', 'http://images6.alphacoders.com/412/412067.jpg', 'https://extension.umass.edu/vegetable/sites/vegetable/files/crops/carrot1.jpg', 'http://cdn.noshon.it/wp-content/uploads/2013-11-04-r-simple-roasted-beet-borscht1.jpg', 'http://continentalicecream.com/wp-content/uploads/2013/10/savoury-Ice-Cream-Cones.jpg', 'http://www.oasisde.com/restaurant/wp-content/uploads/2011/02/falafel-sandwich.jpg', 'http://www.chiantilvpa.com/wp-content/uploads/2014/09/French-fries-deliciouse.jpg', 'http://www.aperitif.no/var/aperitif/storage/images/oppskrifter2/sesongmat/julemenyen/alt-om-lutefisk/302612-23-nor-NO/Alt-om-lutefisk.jpg', 'http://i443.photobucket.com/albums/qq153/jlocklin_photos/Food%20Shots/Beef%20Stroganoff/IMG_4707.jpg', 'http://www.sarahwilson.com.au/wp-content/uploads/2012/03/cream_cheese_16x9.jpg', 'http://riyadhconnect.com/wp-content/uploads/2014/03/chicken-biryani.jpg'];
